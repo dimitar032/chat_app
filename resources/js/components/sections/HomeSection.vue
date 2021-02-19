bbd9c36afca03d53daa84051abdd387b53321c44
Dimitar Dimitrov <ddimitrov@intracol.com>
2021-02-19 14:41:56 +0200

    fix: styling of the home page

--- resources/js/components/sections/HomeSection.vue
+++ resources/js/components/sections/HomeSection.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <p>Hello, {{ userName }}</p>
-    <p>To start a new conversation click on New Message..</p>
+  <div class="col-md-9 row home-applet">
+    <div class="text-center">
+      <p class="hello-text">Hello, {{ userName }}</p>
+      <p class="description-text">
+        To start a new conversation click on New Message..
+      </p>
+    </div>
   </div>
 </template>
 
